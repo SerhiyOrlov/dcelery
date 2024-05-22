c9c270cc8bb395dd74487ae441c7b115da0e93e1
SerhiyOrlov <s.orlov11012@gmail.com>
2024-05-22 20:24:49 +0300

    Minor changes for separated celeryworker

--- app/cworker/tasks.py
+++ app/cworker/tasks.py
@@ -1,5 +1,26 @@
 from celery import shared_task
+from time import sleep
 
 @shared_task
-def sharedtask():
+def tp1(queue='celery'):
+	sleep(3)
 	return
+
+
+@shared_task
+def tp2(queue='celery:1'):
+	sleep(3)
+	return
+
+
+@shared_task
+def tp3(queue='celery:2'):
+	sleep(3)
+	return
+
+
+@shared_task
+def tp4(queue='celery:3'):
+	sleep(3)
+	return
+
