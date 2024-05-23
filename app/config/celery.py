56070dd94cd29a85c6d59ed2b6a1c5ce914abea0
SerhiyOrlov <s.orlov11012@gmail.com>
2024-05-23 08:01:01 +0300

    Added "broker_transport_options" configuration to celery.py. Added commands for creating a group with Celery tasks to "command.md".

--- app/config/celery.py
+++ app/config/celery.py
@@ -4,9 +4,12 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 app = Celery("cworker1")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-
-@app.task
-def add_numbers():
-	return
-
+# app.conf.task_routes = {"cworker.tasks.*": {"queue": "queue1"},
+#                         "cworker.tasks.task2": {"queue": "queue2"},
+# 						}
+app.conf.broker_transport_options = {
+	"priority_steps": list(range(10)),
+	'sep': ":",
+	"queue_ofrder_strategy": "priority"
+}
 app.autodiscover_tasks()
--- commands.md
+++ commands.md
@@ -9,3 +9,16 @@ Key "-t" is for virtual terminal.
 Key "-i" is for using keyboard. 
 
 docker exec -it django /bin/sh
+
+q### Executing a task
+tp1.delay()
+
+### Grouping a tasks
+from celery import group
+
+from cworker.tasks import tp1, tp2, tp3, tp4
+
+tasks_group = group(tp1.s(), tp2.s(), tp3.s(), tp4.s())
+
+tasks_group.apply_async()
+
